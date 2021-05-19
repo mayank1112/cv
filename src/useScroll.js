c6a9c23dd0636f291ad0ab18e0f28c9f0e549e08
mayank1112 <mayank1112@gmail.com>
2021-05-19 15:36:22 +0100

    scoller color change

--- src/useScroll.js
+++ src/useScroll.js
@@ -2,4 +2,5 @@ import { useEffect } from "react";
 
 export const useScroll = (listenToScroll, dependencies) => useEffect(() => {
     window.addEventListener('scroll', listenToScroll);
+    return () => { window.removeEventListener('scroll', listenToScroll) };
 }, dependencies);
